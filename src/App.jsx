a8d68152a310d86a5fac6192ed1d5b4587ce9a80
sumin <pluvial0525@naver.com>
2026-05-14 21:23:14 +0900

    onChange onSubmit

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,103 @@
 import './App.css'
+import { useState } from 'react'
+import ScoresTable from './components/ScoresTable'
 
-const style = {
-  width: "500px",
-  height: "300px",
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  alignItems: "center"
+const initialScores = [
+  { 이름: "John", 국어: 90, 영어: 90, 수학: 80, 과학: 90 },
+  { 이름: "Peter", 국어: 90, 영어: 90, 수학: 80, 과학: 90 },
+  { 이름: "Susan", 국어: 90, 영어: 90, 수학: 80, 과학: 90 },
+  { 이름: "Sue", 국어: 90, 영어: 90, 수학: 80, 과학: 90 }
+]
+
+const initialInfo = {
+  이름: "",
+  국어: null,
+  영어: null,
+  수학: null,
+  과학: null,
 }
 
-
 function App() {
-  const handleChange = (event) =>{
-    console.log(event.target.name)
+
+  const [info, setInfo] = useState(initialInfo)
+  const [scores, setScores] = useState(initialScores)
+
+  const handleChange = (event) => {
+    const { name, value } = event.target
+
+    setInfo((prev) => ({
+      ...prev,
+      [name]: value
+    }))
   }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    setScores((prev) => [...prev, info])
+
+    setInfo(initialInfo)
+  }
+
   return (
-    <form>
-      <div>이름: 
-        <input 
-          type="text" 
-          name="이름" 
-          value=""
-          onChange={handleChange}
+    <>
+      <ScoresTable scores={scores} />
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          이름:
+          <input
+            type="text"
+            name="이름"
+            value={info.이름}
+            onChange={handleChange}
           />
-      </div>
-      <div>국어: 
-        <input 
-          type="number" 
-          name="국어" 
-          value="" 
-          onChange={handleChange}
-        />
-      </div>
-      <div>영어: 
-        <input 
-          type="number" 
-          name="영어" 
-          value="" 
-          onChange={handleChange}
-        />
-      </div>
-      <div>수학: 
-        <input 
-          type="number" 
-          name="수학" 
-          value="" 
-          onChange={handleChange}
-          />
-      </div>
-      <div>과학: 
-        <input 
-          type="number" 
-          name="과학" 
-          value="" 
-          onChange={handleChange}
-        />
         </div>
-      <button>제출</button>
-    </form>
+
+        <div>
+          국어:
+          <input
+            type="number"
+            name="국어"
+            value={info.국어}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div>
+          영어:
+          <input
+            type="number"
+            name="영어"
+            value={info.영어}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div>
+          수학:
+          <input
+            type="number"
+            name="수학"
+            value={info.수학}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div>
+          과학:
+          <input
+            type="number"
+            name="과학"
+            value={info.과학}
+            onChange={handleChange}
+          />
+        </div>
+
+        <button>제출</button>
+      </form>
+    </>
   )
 }
 
-export default App;
+export default App
